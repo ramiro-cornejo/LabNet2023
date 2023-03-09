3c825f0175522c5b72e8f271052e7a3d1b4efa1f
Ramiro Cornejo <ramiro.s.cornejo@gmail.com>
2023-03-08 22:01:17 -0300

    Unit Test
    Se realizo unit test sobre el metodo Dividir

---
+++ LabNetPractica2/LabNetPractica2Tests/Ejercicio02Tests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LabNetPractica2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabNetPractica2.Tests
+{
+    [TestClass()]
+    public class Ejercicio02Tests
+    {
+        [TestMethod()]
+        public void DividirTest()
+        {
+            string num01 = "6";
+            string num02 = "0";
+            
+            string resultadoEsp = "0";
+
+            Ejercicio02 ctrl = new Ejercicio02();
+
+            string resultadoObt = ctrl.Dividir(num01 , num02);
+
+            Assert.AreEqual(resultadoEsp, resultadoObt);
+
+        }
+    }
+}
