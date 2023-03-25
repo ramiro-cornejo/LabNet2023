92e97d56317082f49c0a9a15ab83f7f4656df2eb
Ramiro Cornejo <ramiro.s.cornejo@gmail.com>
2023-03-24 22:40:03 -0300

    Código MVC

--- Lab.EF/Lab.EF.MVC/Controllers/HomeController.cs
+++ Lab.EF/Lab.EF.MVC/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace Lab.EF.MVC.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Descripción de página.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contacta con nuestra empresa.";
 
             return View();
         }
--- Lab.EF/Lab.EF.MVC/Controllers/SuppliersController.cs
+++ Lab.EF/Lab.EF.MVC/Controllers/SuppliersController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Lab.EF.Entities;
+using Lab.EF.Logic.Logic;
+using Lab.EF.MVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +11,90 @@ namespace Lab.EF.MVC.Controllers
 {
     public class SuppliersController : Controller
     {
+        readonly SuppliersLogic suppliersLogic = new SuppliersLogic();
         // GET: Suppliers
         public ActionResult Index()
         {
+
+            List<Suppliers> result = suppliersLogic.GetAll();
+            List<SuppliersViewModel> suppliersView = result.Select(s => new SuppliersViewModel
+            {
+                SupplierID = s.SupplierID,
+                CompanyName = s.CompanyName,
+                ContactName = s.ContactName,
+            }).ToList();
+            return View(suppliersView);
+        }
+        public ActionResult Insert()
+        {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Insert(SuppliersViewModel suppliersView)
+        {
+            try
+            {
+                var supplierInsert = new Suppliers();
+                {
+                    supplierInsert.SupplierID = suppliersView.SupplierID;
+                    supplierInsert.CompanyName = suppliersView.CompanyName;
+                    supplierInsert.ContactName = suppliersView.ContactName;
+                };
+                if(ComprobarSp(supplierInsert))
+                {
+                    suppliersLogic.Add(supplierInsert);
+                    return RedirectToAction("Index");   
+                }
+                else
+                {
+                    throw new Exception();
+                }
+            }
+            catch(Exception ex)
+            {
+                return RedirectToAction("Index", "Error");
+            }
+        }
+        public ActionResult Update()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Update(SuppliersViewModel suppliersView)
+        {
+            try
+            {
+                var supplierUpdate = new Suppliers
+                {
+                    SupplierID = suppliersView.SupplierID,
+                    CompanyName = suppliersView.CompanyName,
+                    ContactName = suppliersView.ContactName
+                };
+                if (ComprobarSp(supplierUpdate))
+                {
+                    suppliersLogic.Update(supplierUpdate);
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    throw new Exception();
+                }
+            }
+            catch(Exception ex)
+            { 
+                return RedirectToAction("Index", "Error");
+            }
+        }
+        public bool ComprobarSp(Suppliers suppliers)
+        {
+            if(!(suppliers.SupplierID.ToString().Any(char.IsDigit)))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
     }
 }
